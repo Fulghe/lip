e0567e95f565108672b05d747d45014ae4a8a50c
Docker <docker@example.com>
2024-10-09 14:25:48 +0100

    Primo esercizio

--- basics/adder/lib/adder.ml
+++ basics/adder/lib/adder.ml
@@ -1,4 +1,5 @@
 (* val addlist : int list -> int *)
 (* addlist l adds the element of the list of integers l *)
 
-let addlist l = 0 (* replace 0 with actual code *)
+let addlist l = List.fold_left( fun sum l1-> sum + l1 ) 0 l;;
+
