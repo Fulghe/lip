af23c86f4473925acd3081ec30519dbba0a0777c
Docker <docker@example.com>
2024-11-06 19:30:19 +0000

    Lezione 5 finito

--- toyparser/lib/main.ml
+++ toyparser/lib/main.ml
@@ -52,3 +52,24 @@ let rec eval : ast -> int_or_err = function
     | _, Error err2 -> Error err2
     | Ok v1, Ok v2 -> Ok (v1 / v2))
     
+(* TASK 6
+    let rec eval : ast -> int_or_err = function
+  | Const n -> Ok n
+  | Add (e1,e2) ->
+      eval e1 ==> fun v1 ->
+      eval e2 ==> fun v2 ->
+        Ok(v1+v2)
+  | Sub (e1,e2) ->
+      eval e1 ==> fun v1 ->
+      eval e2 ==> fun v2 ->
+        Ok(v1-v2)
+  | Mul (e1,e2) ->
+      eval e1 ==> fun v1 ->
+      eval e2 ==> fun v2 ->
+        Ok(v1*v2)
+  | Div (e1,e2) ->
+      eval e1 ==> fun v1 ->
+      eval e2 ==> fun v2 ->
+        if v2 = 0 then Error ("Error: tried to divide " ^ string_of_int v1 ^ " by zero")
+        else Ok(v1/v2)
+        *)
