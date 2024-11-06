10906ff0c737a8a09f937d050f9b2c2ff28e976a
Docker <docker@example.com>
2024-11-06 15:54:09 +0000

    Lezione 5

--- toyparser/lib/ast.ml
+++ toyparser/lib/ast.ml
@@ -1,4 +1,7 @@
 type ast =
-    Const of int
+    Const of int 
   | Add of ast * ast
+  | Sub of ast * ast
+  | Div of ast * ast
+  | Mol of ast * ast
 
--- toyparser/lib/main.ml
+++ toyparser/lib/main.ml
@@ -22,10 +22,33 @@ let string_of_intorerr : int_or_err -> string = function
 
 let rec eval : ast -> int_or_err = function
   | Const n -> Ok n
-  | Add (e1,e2) ->
+  | Add (e1,e2) ->(
     let res1 = eval e1 in
     let res2 = eval e2 in
     match res1, res2 with
     | Error err1, _ -> Error err1
     | _, Error err2 -> Error err2
-    | Ok v1, Ok v2 -> Ok (v1 + v2)
+    | Ok v1, Ok v2 -> Ok (v1 + v2) )
+  | Sub (e3,e4) ->(
+    let res1 = eval e3 in
+    let res2 = eval e4 in
+    match res1, res2 with
+    | Error err1, _ -> Error err1
+    | _, Error err2 -> Error err2
+    | Ok v1, Ok v2 -> Ok (v1 - v2))
+  | Mol (e3,e4) ->(
+    let res1 = eval e3 in
+    let res2 = eval e4 in
+    match res1, res2 with
+    | Error err1, _ -> Error err1
+    | _, Error err2 -> Error err2
+    | Ok v1, Ok v2 -> Ok (v1 * v2))
+  | Div (e3,e4) ->(
+    let res1 = eval e3 in
+    let res2 = eval e4 in
+    match res1, res2 with
+    | _,Ok 0-> failwith "Division by 0 is not possible"
+    | Error err1, _ -> Error err1
+    | _, Error err2 -> Error err2
+    | Ok v1, Ok v2 -> Ok (v1 / v2))
+    
